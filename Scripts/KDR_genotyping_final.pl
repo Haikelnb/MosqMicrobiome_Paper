ec3b9e55a0d5af5667e6cfa926c467ea9629a217
Haikel Bogale <70728342+Haikelnb@users.noreply.github.com>
2020-09-14 17:05:02 -0400

    Add files via upload

---
+++ Scripts/KDR_genotyping_final.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+#This program parses through the primer_sorted files and counts # of sequences that match mutant/WT sequences
+use warnings;
+use strict;
+
+
+my $resultsfilename = $ARGV[0]; 
+open (RESULTSFILE, $resultsfilename) or die "Can't open file '$resultsfilename' [$!]\n";
+my @sample = <RESULTSFILE> ;
+
+
+
+my $KDR_WT = "ACGACTAAATTTC" ;
+my $kdr_w = "ACGACAAAATTTC" ;
+my $kdr_e = "ACGACTGAATTTC" ;
+
+my $prefix = $resultsfilename ;
+$prefix =~ s/\.txt// ;
+
+foreach (@sample) {
+	my @sample_line = split("\t", $_) ;
+	if ($sample_line[3] =~ /$KDR_WT/) {
+		print "\nKDR_WT" ;
+	}
+	elsif ($sample_line[3] =~ /$kdr_w/) {
+		print "\nkdr_w" ;
+	}
+	elsif ($sample_line[3] =~ /$kdr_e/) {
+	  	print "\nkdr_e" ; 
+	}
+	else {
+	  	print "\nother" ;
+	}
+	if ( defined($sample_line[5])) {
+		if ($sample_line[5] =~ /$KDR_WT/) {
+			print "\tKDR_WT\n" ;
+		}
+		elsif ($sample_line[5] =~ /$kdr_w/) {
+			print "\tkdr_w\n" ;
+		}
+		elsif ($sample_line[5] =~ /$kdr_e/) {
+	  		print "\tkdr_e\n" ; 
+		}
+		else {
+	  		print "\tother\n" ;
+		}
+	}
+}
